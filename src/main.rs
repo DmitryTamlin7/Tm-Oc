fe09a030e1b8264419ec3f84135ccfe9e67b6a84
Dmitry <146066734+DmitryTamlin7@users.noreply.github.com>
2026-01-11 19:04:17 +0300

    ЭТАП 6. Heap allocator (Vec, Box, String)

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,63 @@
-#![feature(abi_x86_interrupt)]
 #![no_std]
 #![no_main]
+#![feature(abi_x86_interrupt)]
+#![feature(alloc_error_handler)]
+
+extern crate alloc;
+
+use core::panic::PanicInfo;
+use bootloader::{BootInfo, entry_point};
+use x86_64::VirtAddr;
+use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
 
 mod vga_buffer;
 mod interrupts;
 mod gdt;
-mod memory;
+mod memory; 
 mod allocator;
-mod paging;
 
-use core::panic::PanicInfo;
-use bootloader::bootinfo::BootInfo;
-use x86_64::VirtAddr;
-use crate::memory::BootInfoFrameAllocator;
-use crate::paging::{identity_map_range, init_offset_page_table};
+use crate::memory::{BootInfoFrameAllocator, init_offset_page_table};
 
-#[no_mangle]
-pub extern "C" fn _start(boot_info: &'static BootInfo) -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    println!("Welcome to Tm_Os Heap Test");
+
     gdt::init();
     interrupts::init();
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { init_offset_page_table(phys_mem_offset) };
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
+    
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+        .expect("Heap initialization failed");
+    
+    let x = Box::new(42);
+    println!("Heap works! Value: {}", x);
 
-    println!("OS Initialized!");
+    let mut vec = Vec::new();
+    vec.push(1);
+    vec.push(2);
+    println!("Vector works! {:?}", vec);
 
-    let mut frame_allocator = BootInfoFrameAllocator::init(&boot_info.memory_map);
+    println!("It did not crash!");
 
-    // OffsetPageTable
-    let phys_offset = VirtAddr::new(0);
-    let mut mapper = unsafe { init_offset_page_table(phys_offset) };
-
-    // Identity mapping (например, со 1 MiB, чтобы избежать конфликтов)
-    identity_map_range(0x10_0000, 0x20_0000, &mut mapper, &mut frame_allocator)
-        .expect("Identity mapping failed");
-
-    println!("Paging initialized");
-
-    for i in 0..5 {
-        if let Some(frame) = frame_allocator.allocate_frame() {
-            println!("Allocated frame {} at physical address {:#X}", i, frame.start_address().as_u64());
-        } else {
-            println!("No more frames available!");
-        }
+    loop {
+        x86_64::instructions::hlt();
     }
-
-    // Тест page fault
-    test_page_fault();
-
-    loop {}
 }
 
-fn test_page_fault() {
-    println!("Testing page fault...");
-    unsafe {
-        let ptr = 0xdead_be00 as *mut u64;
-        *ptr = 42;
-    }
+#[alloc_error_handler]
+fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
+    panic!("allocation error: {:?}", layout)
 }
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("PANIC: {}", info);
-    loop {}
+    println!("{}", info);
+    loop {
+        x86_64::instructions::hlt();
+    }
 }
--- src/memory.rs
+++ src/memory.rs
@@ -1,6 +1,28 @@
+use x86_64::{
+    structures::paging::{
+        FrameAllocator, Mapper, OffsetPageTable, Page, PageTable, PageTableFlags, PhysFrame,
+        Size4KiB,
+    },
+    PhysAddr, VirtAddr,
+};
 use bootloader::bootinfo::{MemoryMap, MemoryRegionType};
-use x86_64::structures::paging::{PhysFrame, Size4KiB};
-use x86_64::PhysAddr;
+
+pub unsafe fn init_offset_page_table(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    let level_4_table = active_level_4_table(physical_memory_offset);
+    OffsetPageTable::new(level_4_table, physical_memory_offset)
+}
+
+unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_table_frame, _) = Cr3::read();
+
+    let phys = level_4_table_frame.start_address();
+    let virt = physical_memory_offset + phys.as_u64();
+    let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
+
+    &mut *page_table_ptr
+}
 
 pub struct BootInfoFrameAllocator {
     memory_map: &'static MemoryMap,
@@ -8,38 +30,30 @@ pub struct BootInfoFrameAllocator {
 }
 
 impl BootInfoFrameAllocator {
-
-    pub fn init(memory_map: &'static MemoryMap) -> Self {
+    pub unsafe fn init(memory_map: &'static MemoryMap) -> Self {
         BootInfoFrameAllocator {
             memory_map,
             next: 0,
         }
     }
 
-    fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> + '_ {
-        self.memory_map
-            .iter()
-            .filter(|r| r.region_type == MemoryRegionType::Usable)
-            .flat_map(|r| {
-                let start_frame = PhysFrame::containing_address(PhysAddr::new(r.range.start_addr()));
-                let end_frame = PhysFrame::containing_address(PhysAddr::new(r.range.end_addr() - 1));
-                PhysFrame::range_inclusive(start_frame, end_frame)
-            })
-    }
-
-    pub fn allocate_frame(&mut self) -> Option<PhysFrame> {
-        let frame = self.usable_frames().nth(self.next)?;
-        self.next += 1;
-        Some(frame)
-    }
-
-    pub fn deallocate_frame(&mut self, _frame: PhysFrame) {
-
+    fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> {
+        let regions = self.memory_map.iter();
+        let usable_regions = regions
+            .filter(|r| r.region_type == MemoryRegionType::Usable);
+        
+        let addr_ranges = usable_regions
+            .map(|r| r.range.start_addr()..r.range.end_addr());
+        
+        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
+        frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)))
     }
 }
 
-unsafe impl x86_64::structures::paging::FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
+unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
     fn allocate_frame(&mut self) -> Option<PhysFrame> {
-        self.allocate_frame()
+        let frame = self.usable_frames().nth(self.next);
+        self.next += 1;
+        frame
     }
 }
